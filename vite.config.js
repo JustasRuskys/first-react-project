1a9adf257e1a468614595ec9bfdec250f94123a5
JustasRuskys <justas@expertmedia.lt>
2024-06-11 09:39:20 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/first-react-project/",
   server: {
-    port: 8443,
+    port: 3000,
     proxy: {
       '/api': {
-        target: 'https://JustasRuskys.github.io/first-react-project',
+        target: 'http://localhost:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
